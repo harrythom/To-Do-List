cb088b1fcf2bf2678df594d324657fc111f702ee
Harrison <ht031801@byu.edu>
2022-06-26 17:00:46 -0600

    Project 1

--- TodoList.h
+++ TodoList.h
@@ -1,14 +1,41 @@
-//
-// Created by harrisonhthomas on 6/25/22.
-//
-
 #ifndef TO_DO_LIST_TODOLIST_H
 #define TO_DO_LIST_TODOLIST_H
 
+#include <iostream>
+#include <string>
 
-class TodoList {
+using namespace std;
+
+class TodoList: public TodoListInterface {
+public:
+    TodoList() {
+
+    }
+    virtual ~TodoList() {}
+
+    /*
+    *   Adds an item to the todo list with the data specified by the string "_duedate" and the task specified by "_task"
+    */
+    virtual void add(string _duedate, string _task) = 0;
+
+    /*
+    *   Removes an item from the todo list with the specified task name
+    *
+    *   Returns 1 if it removes an item, 0 otherwise
+    */
+    virtual int remove(string _task) = 0;
+
+    /*
+    *   Prints out the full todo list to the console
+    */
+    virtual void printTodoList() = 0;
+
+    /*
+    *   Prints out all items of a todo list with a particular due date (specified by _duedate)
+    */
+    virtual void printDaysTasks(string _date) = 0;
 
 };
 
 
-#endif //TO_DO_LIST_TODOLIST_H
+#endif
